ea2ec0aca219bea7f749eb3ebab96682f820d2cc
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2022-11-26 19:38:01 +0700

    home page not header

--- src/assets/imgs/index.js
+++ src/assets/imgs/index.js
@@ -1,9 +1,18 @@
 import enjoyDev from './EnjoyDev.jpg'
 import background from './image.jpg'
 import Logo from './logo.jpg'
+import home1 from './home1.jpg'
+import home2 from './home2.jpg'
+import home3 from './home3.jpeg'
+import home4 from './home4.jpg'
+
 
 export {
   enjoyDev,
   background,
-  Logo
+  Logo,
+  home1,
+  home2,
+  home3, 
+  home4
 }
